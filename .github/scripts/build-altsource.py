08d4fa74e51303b26140b582a1cc75d97ac2351e
Cooper <coop@coopeeo.xyz>
2025-05-14 19:35:55 -0500

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- .github/scripts/build-altsource.py
+++ .github/scripts/build-altsource.py
@@ -13,9 +13,9 @@ import copy
 temp_output = Path(os.getenv('OUTPUT'))
 altsourcefolder = temp_output / "altsource"
 altsourcefolder.mkdir(parents=True, exist_ok=True)
-base = json.loads("altsource/base.json")
-baseapp = json.loads("altsource/baseapp.json")
-news = json.loads("altsource/news.json")
+base = json.load("altsource/base.json")
+baseapp = json.load("altsource/baseapp.json")
+news = json.load("altsource/news.json")
 
 base["apps"] = []
 baseapp["versions"] = []
